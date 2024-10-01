14a8b46524f1e11b1f25ef8c4dd6aeb86b4d850a
Judith Claudin <judithclaudin@gmail.com>
2024-10-01 14:02:22 +0200

    new commit

---
+++ front-page.php
@@ -0,0 +1,8 @@
+<?php
+get_header();
+?>
+<div class="image">
+    <h1>PHOTOGRAPHE EVENTS</h1>
+<img width="100%"
+src="http://localhost/motaphoto/wordpress/wp-content/uploads/2024/09/nathalie-11-1.jpeg">
+</div>
---
+++ templates_part/contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+<div class="modal-container">
+  <div class="overlay modal-trigger"></div>
+  <div class="modal">
+  <h1>CONTACTCONTACTCONTACTCONTACTCONTACTCONTACT</h1>
+  <button class="close-modal.modal-trigger">X</button>
+  <a href='[contact-form-7 id="25a3afe" title="CONTACTCONTACTCONTACTCONTACTCONTACTCONTACT"]' class="button">Open</a>
+  </div>
+</div>
+<button class="modal-btn modal-trigger">Open</button>
+ <script src="scripts.js"></script>
+</body>
+</html>
